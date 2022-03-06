211254557a22d4b961948e18f0899f1d20e642ad
Darren Tan <darren243@hotmail.com>
2022-03-06 23:54:11 +0800

    updated inserting line items in profile page

--- client/src/app/profile/profile.component.ts
+++ client/src/app/profile/profile.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { UserInfo } from '../models';
+import { LineItem, UserInfo } from '../models';
+import { UserService } from '../user.services';
 
 
 @Component({
@@ -12,8 +14,11 @@ export class ProfileComponent implements OnInit {
 
   userInfo!: UserInfo
   status: string = ""
+  form!: FormGroup
+  lineItemFormArray!: FormArray
+  listOfLineItem: LineItem[] = []
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private fb: FormBuilder, private userService: UserService) { }
 
   ngOnInit(): void {
     if (window.sessionStorage.getItem("userInfo") === null) {
@@ -21,6 +26,17 @@ export class ProfileComponent implements OnInit {
       this.userInfo = JSON.parse(window.sessionStorage.getItem("userInfo") || "")
       this.status = "login"
     }
+
+    this.lineItemFormArray = this.fb.array([])
+    this.form = this.fb.group({
+      listOfItems: this.lineItemFormArray
+    })
+    if(this.status != "") {
+      for(let i = 0; i < this.userInfo.lineItem.length; i++) {
+        this.listOfLineItem.push(this.userInfo.lineItem[i] as LineItem)
+      }
+    }
+
   }
 
   signOut() {
@@ -28,4 +44,38 @@ export class ProfileComponent implements OnInit {
     this.router.navigate(['/'])
   }
 
+  update() {
+    const listOfItemToUpdate = JSON.parse(JSON.stringify(this.listOfLineItem));
+
+    for(let i = 0; i < this.lineItemFormArray.length; i++) {
+      console.log(this.lineItemFormArray.get(i.toString())!.value.name)
+      console.log(this.lineItemFormArray.get(i.toString())!.value.quantity)
+      let temp = {name: this.lineItemFormArray.get(i.toString())!.value.name, quantity: this.lineItemFormArray.get(i.toString())!.value.quantity } as LineItem
+      listOfItemToUpdate.push(temp)
+    }
+
+    let tempUserInfo = { username: this.userInfo.username, name: this.userInfo.name, email: this.userInfo.email, lineItem: listOfItemToUpdate}
+    window.sessionStorage.setItem("userInfo", JSON.stringify(tempUserInfo))
+    this.userService.updateUser(tempUserInfo)
+      .then(result => {
+        console.log(result)
+      }).catch(error => {console.log(error.message)})
+  }
+
+  addInput() {
+    const lineItemGroup = this.fb.group({
+      name: this.fb.control('', [Validators.required, Validators.minLength(3)]),
+      quantity: this.fb.control('', [Validators.required]),
+    })
+
+    this.lineItemFormArray.push(lineItemGroup)
+  }
+
+  deleteLineItem(itemNumber: number) {
+    this.lineItemFormArray.removeAt(itemNumber)
+  }
+
+  deleteLineItemList(i: number) {
+    this.listOfLineItem.splice(i, 1);
+  }
 }
--- client/src/app/user.services.ts
+++ client/src/app/user.services.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { lastValueFrom } from "rxjs";
-import { User } from "./models";
+import { User, UserInfo } from "./models";
 
 @Injectable()
 export class UserService {
@@ -16,6 +16,12 @@ export class UserService {
   getUser(email: string, password: string) {
     return (lastValueFrom(
       this.http.get<any>(`http://localhost:8080/api/user/login/${email}/${password}`)
+    ))
+  }
+
+  updateUser(userInfo: UserInfo) {
+    return (lastValueFrom(
+      this.http.post<any>("http://localhost:8080/api/user/update", userInfo)
     ))
   }
 }
